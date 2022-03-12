8f618b9960ee81835ae4b0c1bfb646d359547d53
Стас Басов <stas.basov@whatever.com>
2022-03-12 20:26:43 +0500

    Не работают ссылки на группы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -14,10 +14,19 @@ def index(request):
     }
     return render(request, 'posts/index.html', context)
 
-def group_posts(request, pk):
-    template = 'posts/group_list.html'
-    title_post = 'Страница сообщества'
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title_post': title_post,
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
